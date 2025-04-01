5412bdcea281aff1e520c9cfb68d6edb04212278
Aleks Michalak <24192100+aleksishere@users.noreply.github.com>
2025-04-01 11:32:07 +0200

    Fetching products

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import './Shop.css';
+import '../../index.css'
 
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error("Error while fetching", error));
+}, [])
+
   return (
     <>
-      <main>
-        Hello
+      <main className="inter-400">
+        <ul className="shop">
+          {products.map(product => (
+            <div className="shopCard" key={product.id}>
+              <div>
+                <img src={product.image} alt={product.title} />
+              </div>
+              <div className="shopCardDetailWrapper">
+                <div>
+                  <li>{product.title}</li>
+                </div>
+                <div>
+                  <div>
+                    <span>{product.price}$</span>
+                  </div>
+                  <div>
+                    <input defaultValue={1} min={1} type="number" name={product.id} id={product.id} />
+                  </div>
+                </div>
+              </div>
+              <div className="shopCardButton">
+                <button>Add to card</button>
+              </div>
+            </div>
+          ))}
+        </ul>
       </main>
     </>
   )
