2dd0d48594e548e3d5c9d4886d4f07d89c43836d
Aleks Michalak <24192100+aleksishere@users.noreply.github.com>
2025-04-01 10:25:30 +0200

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import TopBar from "./components/TopBar/TopBar.jsx"
 import HomeContent from "./components/HomeContent/HomeContent.jsx"
+import Shop from "./components/Shop/Shop.jsx"
 import Footer from "./components/Footer/Footer.jsx"
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 function App() {
 
   return (
     <>
-      <TopBar />
-      <HomeContent />
-      <Footer />
+      <BrowserRouter>
+        <TopBar />
+        <Routes>
+          <Route path='/' element={<HomeContent />} />
+          <Route path='/shop' element={<Shop />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import './TopBar.css';
 import '../../index.css';
+import { Link } from "react-router";
 
 function TopBar() {
   return (
     <>
       <nav className={`topbar-nav inter-400`}>
         <ul className="topbar-ul">
-        <li>Home</li>
-        <li>Shop</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
         </ul>  
       </nav>
     </>
